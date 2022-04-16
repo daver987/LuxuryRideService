cb06533145b065e367e051eb5dcc98b87d1c2e7c
daver987 <daveyalexander@outlook.com>
2022-04-15 21:42:25 -0400

    fix: placement of iFrame

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,108 +3,106 @@
     <Script src='https://lrs.addons.la/leads/new/forms/resize/expander.php' config='3'></Script>
 
     <TheHeader
-      :aboveHeading="headerInfo.aboveHeading"
-      :body="headerInfo.body"
-      :heading="headerInfo.heading"
-      :image="headerInfo.image"
+      :aboveHeading='headerInfo.aboveHeading'
+      :body='headerInfo.body'
+      :heading='headerInfo.heading'
+      :image='headerInfo.image'
     />
-    <section class="bg-background -mt-12 px-6 pb-12 md:-mt-16 md:pb-16">
-      <div class="container bg-white mx-auto shadow-xl z-10 relative">
-        <div class="py-10 px-6 sm:px-10 lg:col-span-2 xl:p-12">
-          <h3 class="mb-4 subheading">Tell Us Something</h3>
-          <h2 class="text-background-dark heading">Send Us A Message</h2>
-          <div id="contact_form_outer">
-            <div id="contact_form_inner">
-              <FormKit
-                type="form"
-                class="mt-6 grid gap-y-6 grid-cols-1 sm:gap-x-8 sm:grid-cols-2"
-                submit-label="Send Message"
-              >
-                <div>
-                  <FormKit
-                    label="Full Name"
-                    type="text"
-                    name="full-name"
-                    id="full-name"
-                    autocomplete="full-name"
-                    :classes="formClasses"
-                    validation="required"
-                  />
-                </div>
-                <div>
-                  <FormKit
-                    label="Email Address"
-                    id="email"
-                    name="from_email"
-                    type="email"
-                    autocomplete="email"
-                    :classes="formClasses"
-                    validation="required"
-                  />
-                </div>
-                <div>
-                  <FormKit
-                    label="Phone Number"
-                    type="text"
-                    name="phone_number"
-                    id="phone"
-                    autocomplete="tel"
-                    :classes="formClasses"
-                    aria-describedby="phone-optional"
-                  />
-                </div>
-                <div class="sm:col-span-2">
-                  <FormKit
-                    label="Subject"
-                    type="text"
-                    name="subject"
-                    id="subject"
-                    :classes="formClasses"
-                    validation="required"
-                    maxlength="500"
-                  />
-                </div>
+    <section class='bg-background -mt-12 px-6 pb-12 md:-mt-16 md:pb-16'>
+      <div class='container bg-white mx-auto shadow-xl z-10 relative'>
+        <div class='py-10 px-6 sm:px-10 lg:col-span-2 xl:p-12' id='contact_form_outer'>
+          <h3 class='mb-4 subheading'>Tell Us Something</h3>
+          <h2 class='text-background-dark heading'>Send Us A Message</h2>
+          <div id='contact_form_inner'>
+            <FormKit
+              type='form'
+              class='mt-6 grid gap-y-6 grid-cols-1 sm:gap-x-8 sm:grid-cols-2'
+              submit-label='Send Message'
+            >
+              <div>
                 <FormKit
-                  label="Message"
-                  type="textarea"
-                  id="message"
-                  name="message"
-                  rows="4"
-                  :classes="formClasses"
-                  aria-describedby="message-max"
-                  validation="required"
-                  maxlength="500"
+                  label='Full Name'
+                  type='text'
+                  name='full-name'
+                  id='full-name'
+                  autocomplete='full-name'
+                  :classes='formClasses'
+                  validation='required'
                 />
-              </FormKit>
-            </div>
+              </div>
+              <div>
+                <FormKit
+                  label='Email Address'
+                  id='email'
+                  name='from_email'
+                  type='email'
+                  autocomplete='email'
+                  :classes='formClasses'
+                  validation='required'
+                />
+              </div>
+              <div>
+                <FormKit
+                  label='Phone Number'
+                  type='text'
+                  name='phone_number'
+                  id='phone'
+                  autocomplete='tel'
+                  :classes='formClasses'
+                  aria-describedby='phone-optional'
+                />
+              </div>
+              <div class='sm:col-span-2'>
+                <FormKit
+                  label='Subject'
+                  type='text'
+                  name='subject'
+                  id='subject'
+                  :classes='formClasses'
+                  validation='required'
+                  maxlength='500'
+                />
+              </div>
+              <FormKit
+                label='Message'
+                type='textarea'
+                id='message'
+                name='message'
+                rows='4'
+                :classes='formClasses'
+                aria-describedby='message-max'
+                validation='required'
+                maxlength='500'
+              />
+            </FormKit>
           </div>
         </div>
       </div>
     </section>
-    <section class="bg-background mt-16 px-6">
+    <section class='bg-background mt-16 px-6'>
       <div
-        class="container bg-white mx-auto space-y-10 shadow-xl mb-16 py-20 px-12"
+        class='container bg-white mx-auto space-y-10 shadow-xl mb-16 py-20 px-12'
       >
-        <h3 class="text-center subheading">OR REACH US</h3>
-        <h2 class="text-center heading">CONTACT INFORMATION</h2>
+        <h3 class='text-center subheading'>OR REACH US</h3>
+        <h2 class='text-center heading'>CONTACT INFORMATION</h2>
         <div
-          class="flex flex-col justify-center items-center md:grid md:grid-cols-12 md:place-items-center"
+          class='flex flex-col justify-center items-center md:grid md:grid-cols-12 md:place-items-center'
         >
           <div
-            class="mb-12 md:mb-0 md:col-span-5 md:col-start-2 md:row-start-1"
+            class='mb-12 md:mb-0 md:col-span-5 md:col-start-2 md:row-start-1'
           >
             <a
-              href="mailto:info@luxuryrideservice.com"
-              class="text-heading text-lg tracking-wide"
+              href='mailto:info@luxuryrideservice.com'
+              class='text-heading text-lg tracking-wide'
             >
-              <div class="mx-auto text-body mb-8 text-9xl i-carbon-email" />
+              <div class='mx-auto text-body mb-8 text-9xl i-carbon-email' />
               info@luxuryrideservice.com</a
             >
           </div>
-          <div class="col-span-4 col-start-7 row-start-1">
-            <a href="tel:2894009408" class="text-lg tracking-wide text-heading">
+          <div class='col-span-4 col-start-7 row-start-1'>
+            <a href='tel:2894009408' class='text-lg tracking-wide text-heading'>
               <div
-                class="mx-auto text-center text-body mb-6 text-9xl i-carbon-phone"
+                class='mx-auto text-center text-body mb-6 text-9xl i-carbon-phone'
               />
               289.400.9408</a
             >
@@ -116,12 +114,12 @@
   </div>
 </template>
 
-<script setup lang="ts">
+<script setup lang='ts'>
 
 const headerInfo = {
   aboveHeading: 'GET IN TOUCH',
   heading: 'CONTACT US',
-  body: "Your chauffeur is just a phone call away! Don't hesitate to contact us or get an online quote for rates and availability.",
+  body: 'Your chauffeur is just a phone call away! Don\'t hesitate to contact us or get an online quote for rates and availability.',
   image: 'background-image: url("/images/View-City-Of-Toronto.jpg")',
 }
 
