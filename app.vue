9fe591d21eb46cb44771fee972cfc08e03b62688
daver987 <daveyalexander@outlook.com>
2022-04-21 04:42:37 -0400

    ass: app.vue for globals

---
+++ app.vue
@@ -0,0 +1,39 @@
+<script setup lang='ts'>
+useHead({
+  title: 'Toronto Car Service | Luxury Ride Service',
+  meta: [
+    {
+      name: 'description',
+      content:
+        'Luxury Ride Service a premier Toronto car service. Luxury cars for hire, Tour Services, Limousine services and airport transfers at affordable prices.',
+    },
+    {
+      name: 'google-site-verification',
+      content: 'vxHnCltMFx06rPY1w0vXooUDj8NjRfXmIHbOREMUxrQ',
+    },
+  ],
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: '/images/favicon.png' },
+    {
+      rel: 'canonical',
+      href: 'https://luxuryrideservice.com',
+      crossorigin: true,
+    },
+  ],
+})
+</script>
+
+<template>
+  <div>
+    <TheNavbar />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+    <TheFooter />
+  </div>
+</template>
+
+
+<style>
+
+</style>
