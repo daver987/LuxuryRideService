1cf38c885790964a65f1453f05a9089700a9ea5a
daver987 <daveyalexander@outlook.com>
2022-05-09 01:22:23 -0400

    Add: Static Hero on index page

---
+++ components/StaticHero.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="bg-center bg-cover bg-no-repeat h-screen px-6 z-10 overflow-hidden relative"
+    style="background-image: url('/icons/gradient-background.svg')"
+  >
+    <div
+      class="grid grid-cols-2 items-center justify-center h-full max-w-7xl mx-auto"
+    >
+      <div class="col-span-1 p-4">
+        <h1 class="heading text-white">{{ slide.heading }}</h1>
+        <p class="subheading">{{ slide.subHeading }}</p>
+        <img
+          :alt="slide.altText"
+          :class="slide.style"
+          :src="slide.image"
+          class="h-full object-center object-cover w-full"
+        />
+      </div>
+      <div class="col-span-1 pl-16">
+        <LeadForm />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const slide = {
+  heading: 'Experience Luxury Rides and Tours',
+  subHeading: 'In Toronto',
+  image: '/images/Cadillac-XTS-Car-Service.png',
+  altText: 'Cadillac XTS Car Service',
+  buttonText: 'Book Now',
+  buttonLink: '/reservations',
+  id: '1',
+  style: 'transform rotate-y-180',
+}
+</script>
