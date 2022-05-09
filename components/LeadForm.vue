bfde027cc7b5d95dbc5dfb67fef7266f3ea5920d
daver987 <daveyalexander@outlook.com>
2022-05-09 01:21:59 -0400

    Add: Lead Form Component

---
+++ components/LeadForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <Script
+    src="https://lrs.addons.la/leads/new/forms/resize/expander.php"
+    :config="config"
+  ></Script>
+  <div class="lead_form_outer w-full h-full">
+    <div id="lead_form_inner" class="w-full h-full"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  config: {
+    type: Number,
+    default: 9,
+  },
+})
+</script>
