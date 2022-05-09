aa98778f7fbe1ff00a2304c28d72d9fb4ecf6b80
daver987 <daveyalexander@outlook.com>
2022-05-09 02:16:22 -0400

    Add: props to lead form

--- components/LeadForm.vue
+++ components/LeadForm.vue
@@ -3,8 +3,8 @@
     src="https://lrs.addons.la/leads/new/forms/resize/expander.php"
     :config="config"
   ></Script>
-  <div class="lead_form_outer w-full h-full relative z-100">
-    <div id="lead_form_inner" class="w-full h-full"></div>
+  <div :class="outer" class="w-full h-full relative z-100">
+    <div :id="inner" class="w-full h-full"></div>
   </div>
 </template>
 
@@ -13,6 +13,14 @@ defineProps({
   config: {
     type: Number,
     default: 9,
+  },
+  outer: {
+    type: String,
+    default: 'lead_form_outer',
+  },
+  inner: {
+    type: String,
+    default: 'lead_form_inner',
   },
 })
 </script>
