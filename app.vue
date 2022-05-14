0f94af8cdad6e2d9d89d0f56d15d37287b809dcb
daver987 <daveyalexander@outlook.com>
2022-05-14 19:58:35 -0400

    Add: some meta attributes

--- app.vue
+++ app.vue
@@ -7,6 +7,36 @@ useHead({
       content:
         'Luxury Ride Service is a premier Toronto car service. Tour Services, Limousine services and airport transfers at affordable prices.',
     },
+    {
+      name: 'robots',
+      content:
+        'index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1',
+    },
+    {
+      property: 'og:locale',
+      content: 'en_CA',
+    },
+    {
+      property: 'og:type',
+      content: 'website',
+    },
+    {
+      property: 'og:title',
+      content: 'Toronto Car Service | Luxury Ride Service',
+    },
+    {
+      property: 'og:description',
+      content:
+        'Luxury Ride Service is a premier Toronto car service. Tour Services, Limousine services and airport transfers at affordable prices.',
+    },
+    {
+      property: 'og:url',
+      content: 'https://luxuryrideservice.com/',
+    },
+    {
+      property: 'og:site_name',
+      content: 'Luxury Ride Service | Toronto Car Service',
+    },
   ],
   link: [
     { rel: 'icon', type: 'image/x-icon', href: '/images/favicon.png' },
